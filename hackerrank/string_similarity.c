ad081e8f793beb35dda032f407bc19da031761c6
ukarim <248163264@bk.ru>
2021-01-03 12:25:52 +0600

    z-algorithm for string similarity

--- hackerrank/string_similarity.c
+++ hackerrank/string_similarity.c
@@ -9,8 +9,9 @@
 #define BUF_SIZE 100001
 
 
-int calc_similarity(const char *str);
+long int calc_similarity(const char *str);
 
+/* slow version O(n^2)
 int calc_similarity(const char *str) {
   int res = 0;
   size_t len = strlen(str);
@@ -31,13 +32,55 @@ int calc_similarity(const char *str) {
   }
   return res;
 }
+*/
 
+// z algorithm O(n)
+
+long int calc_similarity(const char *str) {
+  size_t len = strlen(str);
+  long int z[len];
+  z[0] = len; // whole string is first suffix
+
+  size_t l, r = 0;
+
+  for (size_t i = 1; i < len; i++) {
+    if (i > r) {
+      // trivial search
+      l = i;
+      r = i;
+      while (r < len && str[r - l] == str[r]) {
+        r++;
+      }
+      z[i] = r - l;
+      r--;
+    } else {
+      // inside previous suffix
+      size_t k = i - l;
+      if (z[k] < r - i + 1) {
+        z[i] = z[k];
+      } else {
+        l = i;
+        while (r < len && str[r - l] == str[r]){
+          r++;
+        }
+        z[i] = r - l;
+        r--;
+      }
+    }
+  }
+
+  long int res = 0;
+  for (size_t i = 0; i < len; i++) {
+    res += z[i];
+  }
+  return res;
+}
 
 int main(void) {
   int t;
   scanf("%d", &t);
   
-  int res[t];
+  long int res[t];
   char buf[BUF_SIZE];
   for (int i = 0; i < t; i++) {
     memset(buf, 0, BUF_SIZE);
@@ -46,7 +89,7 @@ int main(void) {
   }
 
   for (int i = 0; i < t; i++) {
-    printf("%d\n", res[i]);
+    printf("%ld\n", res[i]);
   }
 }
 
